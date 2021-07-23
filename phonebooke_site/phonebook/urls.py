6a6b1eac735ff5768b3f2f3e1013ba4104a8c967
BEPb <57312267+BEPb@users.noreply.github.com>
2021-07-23 22:36:58 +0300

    add base.html

--- phonebooke_site/phonebook/urls.py
+++ phonebooke_site/phonebook/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),  # при запросе в браузере пустрой строки отображается страница index
-    path('index/', index),  # при запросе в браузере index отображается страница index
-    path('test_list/', test_list),  # при запросе в браузере test_list отображается страница test_list
-    path('test/', test),  # при запросе в браузере test отображается страница test
+    path('', index, name='home'),  # при запросе в браузере пустрой строки отображается страница index
+    path('index/', index, name='home'),  # при запросе в браузере index отображается страница index
+    path('test_list/', test_list, name='test_list'),  # при запросе в браузере test_list отображается страница test_list
+    path('test/', test, name='test'),  # при запросе в браузере test отображается страница test
 ]
