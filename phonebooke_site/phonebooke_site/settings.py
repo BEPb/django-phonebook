6ecbba6cb3e7f6165daa23a4491de3391cdadbae
BEPb <57312267+BEPb@users.noreply.github.com>
2021-07-23 21:34:36 +0300

    add js bootstrap

--- phonebooke_site/phonebooke_site/settings.py
+++ phonebooke_site/phonebooke_site/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+# подключаем статические маршруты для работы с bootstrap
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
