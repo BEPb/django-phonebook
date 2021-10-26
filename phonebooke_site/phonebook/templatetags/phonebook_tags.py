529b3db6e53c3850f609d9320a196d2ebbb40c8f
BEPb <andrej.marinchenko@gmail.com>
2021-10-26 23:20:14 +0300

    26.10.2021 remember all

--- phonebooke_site/phonebook/templatetags/phonebook_tags.py
+++ phonebooke_site/phonebook/templatetags/phonebook_tags.py
@@ -7,7 +7,7 @@ from phonebook.models import *  # импорт всех классов из фа
 
 register = template.Library()
 
-@register.simple_tag()  # указываем простой декоратор (simple_tag) и присваеваем ему имя
+@register.simple_tag()  # указываем простой декоратор взятия списка всех воинских частей (simple_tag) и присваеваем ему имя
 def get_units():   # задаем пользовательский тег
     # return MilitaryUnit.objects.all()  # отображает список всех воинских частей
     return MilitaryUnit.objects.annotate(cnt=Count('phonenumber')).filter(cnt__gt=0)  # отображает только в\ч в которых есть записи
@@ -18,7 +18,7 @@ def get_division():   # задаем пользовательский тег
 
 
 @register.inclusion_tag('phonebook/list_divisions.html')  # указываем встроенный декоратор
-def show_divisions():  # задаем пользовательский тег
+def show_divisions():  # задаем пользовательский тег отобразить подразделения
     # divisions = Division.objects.all()  # загружает полный список таблицы divisions
     # divisions = Division.objects.annotate(cnt=Count('phonenumber')).filter(cnt__gt=0) #загружаем не пустые списки
     #divisions = Division.objects.annotate(cnt=Count('phonenumber', filter=F('phonenumber__is_published'))).filter(cnt__gt=0).filter(phonenumber__military_unit_id=1)
@@ -34,3 +34,4 @@ def show_all():  # задаем пользовательский тег
     units = MilitaryUnit.objects.all()
     line = Phonenumber.objects.filter(id=8)
     return {"phonebooks": phonebooks, "divisions": divisions, "units": units, "line": line}
+
